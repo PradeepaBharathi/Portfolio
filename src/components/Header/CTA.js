276e300897c08023202f2584f14e73e129c5f0cb
PradeepaBharathi <bharathi83201@gmail.com>
2023-11-15 18:37:39 +0530

    updates

--- src/components/Header/CTA.js
+++ src/components/Header/CTA.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import FinalCV from '../../Assest/Final_CV_Pradeepa.pdf'
+import CV from '../../Assest/CV (2).pdf'
 function CTA() {
   return (
       <div className='cta'>
-          <a href={FinalCV} download className='btn'>Download CV</a>
+          <a href={CV} download className='btn'>Download CV</a>
           <a href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
   )
